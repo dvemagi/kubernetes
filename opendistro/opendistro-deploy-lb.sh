03eecef8a4ce8de5ad5cee6376cac3ff299b67ab
Dario Vemagi <d.vemagi@nanosoft.it>
2020-04-22 22:47:40 +0200

    bash

--- opendistro/opendistro-deploy-lb.sh
+++ opendistro/opendistro-deploy-lb.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-namespace="od-ics-test"
+namespace="$1"
+if  ["$namespace" = "" ]
+    then 
+        echo "Inserire in nome del namespace"
+        exit
+    fi
 
-# Fa il deploy dei container
-#helm -n "$1" install opendistro-es-1.6.0.tgz --generate-name -f opendistro-values.yaml
-
-# prendo il nome della release
-helm_release="$1"
+# Fa il deploy dei container e carica il nome della release nella variabile
+helm_release="helm -n $1 install opendistro-es-1.6.0.tgz --generate-name -f opendistro-values.yaml | awk '/NAME:/ { print $2 }' -"
 
 # sostituisco il valore nel template LB di elastic
-template=`cat "opendistro-elastic-lb.yaml.template" | sed "s/{{helm_release}}/$helm_release/g"` 
+template_elastic=`cat "opendistro-elastic-lb.yaml.template" | sed "s/{{helm_release}}/$helm_release/g"` 
 
 # effettuo il deploy del LB di elastic
-echo "$template" | kubectl -n "$namespace" apply -f -
+echo "$template_elastic" | kubectl -n "$namespace" apply -f -
 
 # sostituisco il valore nel template LB di kibana
-template=`cat "opendistro-kibana-lb.yaml.template" | sed "s/{{helm_release}}/$helm_release/g"` 
+template_kibana=`cat "opendistro-kibana-lb.yaml.template" | sed "s/{{helm_release}}/$helm_release/g"` 
 
 # effettuo il deploy del LB di elastic
-echo "$template" | kubectl -n "$namespace" apply -f -
+echo "$template_kibana" | kubectl -n "$namespace" apply -f -
 
 
