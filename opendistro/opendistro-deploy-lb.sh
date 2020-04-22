0bf3a9cf3e64b7eee803b9385741ae053e0347cc
Dario Vemagi <d.vemagi@nanosoft.it>
2020-04-22 18:17:01 +0200

    elastic e kibana

--- opendistro/opendistro-deploy-lb.sh
+++ opendistro/opendistro-deploy-lb.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-# Fa il deploy dei container
-#helm -n od-ics-test install opendistro-es-1.6.0.tgz --generate-name --set elasticsearch.data.replicas=2 --set elasticsearch.client.replicas=2 --set elasticsearch.data.storage="50Gi"
+#namespace="$1"
 
-# prendo il valore 
+# Fa il deploy dei container
+#helm -n "$1" install opendistro-es-1.6.0.tgz --generate-name -f opendistro-values.yaml
+
+# prendo il nome della release
 helm_release="$1"
 
 # sostituisco il valore nel template LB di elastic
 template=`cat "opendistro-elastic-lb.yaml.template" | sed "s/{{helm_release}}/$helm_release/g"` 
+
+# effettuo il deploy del LB di elastic
+echo "$template" 
+#| kubectl apply -f -
+
+# sostituisco il valore nel template LB di kibana
+template=`cat "opendistro-kibana-lb.yaml.template" | sed "s/{{helm_release}}/$helm_release/g"` 
 
 # effettuo il deploy del LB di elastic
 echo "$template" 
